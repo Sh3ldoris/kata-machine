e89a18183fa622d0575a5fe4813e60c3bb3b2bc0
Adam Lany <adam.lany@posam.sk>
2025-05-25 21:14:44 +0200

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,41 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    #head?: QNode<T>;
 
     constructor() {
+        this.#head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
 
-}
+        const newNode: QNode<T> = {
+            value: item,
+            next: this.#head,
+        };
+
+        this.#head = newNode;
+    }
+
     pop(): T | undefined {
+        if (!this.#head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head: QNode<T> = this.#head;
+        this.#head = this.#head?.next;
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.#head?.value;
+    }
 }
