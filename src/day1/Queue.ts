1e1df0f548bcc7ada0fd3dfb43f7daa69cd10719
Adam Lany <adam.lany@posam.sk>
2025-05-25 20:29:06 +0200

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,9 +1,50 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
+
 export default class Queue<T> {
     public length: number;
+    private head?: QNode<T>; // First added item
+    private tail?: QNode<T>; // Last added item
 
-    constructor() {}
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+    enqueue(item: T): void {
+        const newNode: QNode<T> = {
+            value: item,
+        };
+
+        if (!this.tail) {
+            this.tail = this.head = newNode;
+        }
+        this.length++;
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+
+    deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
