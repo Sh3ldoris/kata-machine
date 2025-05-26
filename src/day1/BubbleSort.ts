2e2c1dcf127dd4dfa685b802da7c04f102fb2068
Adam Lany <adam.lany@posam.sk>
2025-05-26 21:39:08 +0200

    Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,2 +1,13 @@
+// O(n^2)
 export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
