9bcb36374a3917a12f37ecc554b048fa626e71a7
Adam Lany <adam.lany@posam.sk>
2025-05-29 21:49:37 +0200

    Tree pre-order traversal non-recursive

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -9,9 +9,49 @@ function walk(node: BinaryNode<number> | null, path: number[]): void {
     walk(node.right, path);
 }
 
+function walk_non_recursive(
+    node: BinaryNode<number> | null,
+    path: number[],
+): void {
+    if (!node) {
+        return;
+    }
+    if (!node.right && !node.left) {
+        path.push(node.value);
+        return;
+    }
+
+    const stack: BinaryNode<number>[] = [];
+    let cur: BinaryNode<number> | undefined = node;
+    let hasPopped: boolean = false;
+    do {
+        if (!hasPopped && cur.left) {
+            path.push(cur.value);
+            stack.push(cur);
+            cur = cur.left as BinaryNode<number>;
+            continue;
+        }
+        if ((!cur.left || hasPopped) && cur.right) {
+            if (!hasPopped) {
+                path.push(cur.value);
+            }
+            hasPopped = false;
+            cur = cur.right as BinaryNode<number>;
+            continue;
+        }
+        if ((!cur.left || hasPopped) && !cur.right) {
+            if (!hasPopped) {
+                path.push(cur.value);
+            }
+            cur = stack.pop();
+            hasPopped = true;
+        }
+    } while (cur);
+}
+
 export default function pre_order_search(head: BinaryNode<number>): number[] {
     const path: number[] = [];
-    walk(head, path);
+    walk_non_recursive(head, path);
 
     return path;
 }
