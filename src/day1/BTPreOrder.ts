fdb4df99840810092dd98afec29c40be5ede3c60
Adam Lany <adam.lany@posam.sk>
2025-05-29 21:13:35 +0200

    Tree pre-order traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): void {
+    if (!node) {
+        return;
+    }
+    // Log the value
+    path.push(node.value);
+    // Recurse to its children
+    walk(node.left, path);
+    walk(node.right, path);
+}
 
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+
+    return path;
 }
