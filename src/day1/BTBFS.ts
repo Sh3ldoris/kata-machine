3ccbcd0e5b6d83fb774740b31da23ca9e9c98e3f
Adam Lany <adam.lany@posam.sk>
2025-06-01 10:36:16 +0200

    Breath-first tree search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,23 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "@code/Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: Queue<BinaryNode<number>> = new Queue<BinaryNode<number>>();
+    // Insert head
+    queue.enqueue(head);
+    while (queue.length) {
+        const curr: BinaryNode<number> = queue.deque() as BinaryNode<number>;
+
+        if (curr.left) {
+            queue.enqueue(curr.left);
+        }
+        if (curr.right) {
+            queue.enqueue(curr.right);
+        }
+
+        if (curr.value === needle) {
+            return true;
+        }
+    }
+
+    return false;
 }
