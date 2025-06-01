2598fa8bbf70bb37323954fd94eeed19d98f6c8a
Adam Lany <adam.lany@posam.sk>
2025-06-01 10:13:28 +0200

    Tree post-order traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,17 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): void {
+    if (!node) {
+        return;
+    }
+    // Recurse to its children
+    walk(node.left, path);
+    walk(node.right, path);
+    // Log the value
+    path.push(node.value);
+}
 
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+
+    return path;
 }
